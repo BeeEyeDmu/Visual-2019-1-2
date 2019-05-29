1e4830275167c464abddac086a2763e861cdd1e0
BeeEye Dmu <bikang@konyang.ac.kr>
2019-05-29 12:52:23 +0900

    아날로그 시계 추가
    아날로그 시계 코드 추가

--- A028_FormClock/Form1.cs
+++ A028_FormClock/Form1.cs
@@ -12,26 +12,116 @@ namespace A028_FormClock
 {
   public partial class Form1 : Form
   {
-    Timer t = new Timer();
-    Graphics g;
+    private Timer t = new Timer();
+    private Graphics g;
+    private double radius; // 반지름
+    private Point center; // 중심점
+    private int hourHand; // 시침 길이
+    private int minHand;  // 분침
+    private int secHand;  // 초침
+    private const int clientSize = 300; // 화면크기
+    private const int clockSize = 200;  // 시계 크기
 
     public Form1()
     {
       InitializeComponent();
 
-      panel1.Size = new Size(300, 300);
-      this.ClientSize = new Size(300, 300 + menuStrip1.Height);
+      panel1.Size = new Size(clientSize, clientSize);
+      this.ClientSize = new Size(clientSize, clientSize + menuStrip1.Height);
+      this.Text = "나의 시계";
 
       t.Interval = 1000;
       t.Tick += T_Tick;
       t.Start();
 
       g = panel1.CreateGraphics();
+      aClockSetting();
+    }
+
+    private void aClockSetting()
+    {
+      center = new Point(panel1.Width / 2, panel1.Height / 2);
+      radius = panel1.Width / 2;
+
+      hourHand = (int)(radius * 0.45);
+      minHand = (int)(radius * 0.55);
+      secHand = (int)(radius * 0.65);
     }
 
     // 현재시간에 맞추어 아날로그 시계를 그린다
     private void T_Tick(object sender, EventArgs e)
     {
+      DateTime c = DateTime.Now;
+      panel1.Refresh();
+      DrawClockFace();  // 시계판 그리기
+
+      // 시,분,초침의 각도를 계산
+      double radHr 
+        = (c.Hour % 12 + c.Minute / 60.0) * 30 * Math.PI / 180;
+      double radMin
+        = (c.Minute + c.Second / 60.0) * 6 * Math.PI / 180;
+      double radSec
+        = c.Second * 6 * Math.PI / 180;
+
+      DrawHands(radHr, radMin, radSec);
+
+      // 배꼽
+      int coreSize = 16;
+      Rectangle r = new Rectangle(center.X - coreSize / 2,
+        center.Y - coreSize / 2, coreSize, coreSize);
+      g.FillEllipse(Brushes.Gold, r);
+      Pen p = new Pen(Brushes.DarkRed, 3);
+      g.DrawEllipse(p, r);
+    }
+
+    // 시계바늘
+    private void DrawHands(double radHr, double radMin, double radSec)
+    {
+      // 시침
+      DrawLine((int)(hourHand*Math.Sin(radHr)), 
+        (int)(-hourHand*Math.Cos(radHr)),
+        0, 0, Brushes.RoyalBlue, 8, center.X, center.Y);
+      // 분침
+      DrawLine((int)(minHand * Math.Sin(radMin)),
+        (int)(-minHand * Math.Cos(radMin)),
+        0, 0, Brushes.SkyBlue, 6, center.X, center.Y);
+      // 초침
+      DrawLine((int)(secHand * Math.Sin(radSec)),
+        (int)(-secHand * Math.Cos(radSec)),
+        0, 0, Brushes.OrangeRed, 3, center.X, center.Y);
+    }
+
+    private void DrawLine(int x1, int y1, int x2, int y2, Brush color, int thick, int cx, int cy)
+    {
+      Pen pen = new Pen(color, thick);
+      pen.StartCap = System.Drawing.Drawing2D.LineCap.Round;
+      pen.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+      g.DrawLine(pen, x1 + cx, y1 + cy, x2 + cx, y2 + cy);
+    }
+
+    // 시계판
+    private void DrawClockFace()
+    {
+      Pen pen = new Pen(Brushes.LightSteelBlue, 30);
+      g.DrawEllipse(pen,
+        center.X - clockSize / 2,
+        center.Y - clockSize / 2,
+        clockSize, clockSize);
+
+      for(int i=0; i<12; i++)
+      {
+        int deg = i * 30;
+        double rad = deg * Math.PI / 180;
+        int x = (int)(center.X + clockSize/2 * Math.Sin(rad));
+        int y = (int)(center.Y - clockSize/2 * Math.Cos(rad));
+        Rectangle r = new Rectangle(x - 3, y - 3, 6, 6);
+        g.FillEllipse(Brushes.Blue, r);
+      }
+    }
+  }
+}
+
+/* 
       int sec = DateTime.Now.Second;  // 초
       double secDeg = sec * 6;    // 초침이 12시방향과 이루는 각도
       // 초침의 길이는 200
@@ -58,6 +148,4 @@ namespace A028_FormClock
 
       g.DrawLine(new Pen(Color.Blue), new Point(150, 150),
         new Point((int)(150 + x2), (int)(150 - y2)));
-    }
-  }
-}
+        */
