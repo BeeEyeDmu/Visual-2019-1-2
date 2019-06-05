aa537c923a2dbd0515115085819df3d0ee534c62
BeeEye Dmu <bikang@konyang.ac.kr>
2019-06-05 12:52:19 +0900

    2019.6.5 2분반 수업

---
+++ A031_ExclusiveRandom/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A031_ExclusiveRandom
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int[] a = new int[16];
+      
+      // 16개의 중복되지 않는 랜덤 숫자를 저장한다
+      for(int i=0; i<16; i++)
+      {
+        a[i] = Check()%8;
+      }
+
+      for (int i = 0; i < 16; i++)
+      {
+        Console.WriteLine(a[i]);
+      }
+    }
+
+    static bool[] check = new bool[16];
+
+    // 중복되지 않은 랜덤 값을 리턴하는 메서드
+    private static int Check()
+    {
+      Random r = new Random();
+      
+      while(true)
+      {
+        int x = r.Next(16); // 0~15
+        if(check[x] == false)
+        {
+          check[x] = true;
+          return x;
+        }
+      }
+    }
+  }
+}
---
+++ A032_MatchingGame/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace A032_MatchingGame
+{
+  /// <summary>
+  /// MainWindow.xaml에 대한 상호 작용 논리
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+
+      BoardSet();
+    }
+
+    private void BoardSet()
+    {
+      for(int i=0; i<16; i++)
+      {
+        Button b = new Button();
+        b.Background = Brushes.White;
+        b.Margin = new Thickness(10);
+        b.Tag = SetRandom() % 8;
+        b.Content = MakeImage("../../Images/check.png");
+        board.Children.Add(b);
+      }
+    }
+
+    private Image MakeImage(string v)
+    {
+      BitmapImage bi = new BitmapImage();
+      bi.BeginInit();
+      bi.UriSource = new Uri(v, UriKind.Relative);
+      bi.EndInit();
+
+      Image myImage = new Image();
+      myImage.Source = bi;
+      return myImage;
+    }
+
+    bool[] check = new bool[16];
+
+    private int SetRandom()
+    {
+      Random r = new Random();
+
+      while (true)
+      {
+        int x = r.Next(16); // 0~15
+        if (check[x] == false)
+        {
+          check[x] = true;
+          return x;
+        }
+      }
+    }
+  }
+}
