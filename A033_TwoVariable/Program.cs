5422560bd1e8147f29c8c44f63aa719db4b09eea
BeeEye Dmu <bikang@konyang.ac.kr>
2019-06-11 12:02:16 +0900

    수정

---
+++ A033_TwoVariable/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A033_TwoVariable
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int x = 10;
+      int y = 20;
+      Console.WriteLine(x);
+      Console.WriteLine(y);
+
+      // 1. string으로 바꾸어 출력
+      Console.WriteLine(x.ToString() + ", " + y.ToString());
+      Console.WriteLine(x + ", " + y);
+
+      // 2. 
+      Console.WriteLine("{0}, {1}", x, y);
+
+      // 3. 문자열 보간(String Interpolation)
+      // C# 6.0
+      Console.WriteLine($"{x}, {y}");
+    }
+  }
+}
